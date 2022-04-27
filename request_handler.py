de076c30b2d4cda886e2f2d576b719e8c7768539
Hilary Shaner <hilaryshaner@gmail.com>
2022-04-27 11:34:43 -0500

    able to add a new category

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views.category_requests import get_all_categories
+from views.category_requests import create_category, get_all_categories
 from views.post_requests import get_all_posts
 from views.tag_requests import delete_tag, edit_tag, get_all_tags, get_single_tag
 from views.user_requests import create_user, login_user
@@ -85,6 +85,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             response = login_user(post_body)
         if resource == 'register':
             response = create_user(post_body)
+        if resource == "categories":
+            response = create_category(post_body)
 
         self.wfile.write(response.encode())
 
